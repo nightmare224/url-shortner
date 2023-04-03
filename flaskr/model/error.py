01df43d06be321a987be9c825940a2f95178701f
Nicole Tai <nicole1999224@gmail.com>
2023-04-04 00:48:40 +0200

    add error handle model

---
+++ flaskr/model/error.py
@@ -0,0 +1,15 @@
+class BadRequest(Exception):
+    status_code = 400
+    def __init__(self, message = None):
+        super().__init__()
+        self.payload = {
+            "error": str(message) if message else "404 Not Found."
+        }
+
+class NotFound(Exception):
+    status_code = 404
+    def __init__(self, message = None):
+        super().__init__()
+        self.payload = {
+            "error": str(message) if message else "400 Bad Request."
+        }
