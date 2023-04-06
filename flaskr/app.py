2b529b78706d1327687413cf8931cbc27d0555f1
Nicole Tai <nicole1999224@gmail.com>
2023-04-06 11:18:18 +0200

    flasgger add schema and ID model change

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,14 +1,29 @@
 from flask import Flask
-from flasgger import Swagger
+from flasgger import Swagger, APISpec
+from apispec.ext.marshmallow import MarshmallowPlugin
+from apispec_webframeworks.flask import FlaskPlugin
 from route.url import url_restapi
-from route.id import id_restapi
+from route.shorturl import shorturl_restapi
 from route.error_controller import error_controller
+from schema.shorturl import ShortURLSchema
+from schema.url import URLSchema
 
 
 app = Flask(__name__)
-# For Documatation
-swagger = Swagger(app)
+
+# For Documatation (flasgger configuration)
+spec = APISpec(
+    title='Shorten URL APIs',
+    version='1.0.0',
+    openapi_version='2.0',
+    plugins=[
+        FlaskPlugin(),
+        MarshmallowPlugin(),
+    ],
+)
+template = spec.to_flasgger(app, definitions=[ShortURLSchema, URLSchema])
+swagger = Swagger(app, template = template)
 
 app.register_blueprint(url_restapi)
-app.register_blueprint(id_restapi)
+app.register_blueprint(shorturl_restapi)
 app.register_blueprint(error_controller)
