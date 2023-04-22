b4cff088f8d1bf687ee1893157b6b14e3d8c911a
yssamtu <yssamtu@gmail.com>
2023-04-23 00:12:57 +0200

    change return value from url_id to short_url_id

--- url-shortener/api/lib/shortener.py
+++ url-shortener/api/lib/shortener.py
@@ -12,31 +12,30 @@ def base62_encode(url_id) -> str:
 
     return short_url_id if short_url_id else base62_values[0]
 
-def base62_decode(short_url_id) -> int:
-    base62_values = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    base = 62
-    url_id = 0
-    for i, letter in enumerate(short_url_id[::-1]):
-        val = base62_values.index(letter)
-        url_id = url_id + val * base**i
+# def base62_decode(short_url_id) -> int:
+#     base62_values = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+#     base = 62
+#     url_id = 0
+#     for i, letter in enumerate(short_url_id[::-1]):
+#         val = base62_values.index(letter)
+#         url_id = url_id + val * base**i
 
-    return url_id
+#     return url_id
 
 
-def query_next_unique_id(full_url) -> int:
+def query_next_unique_id(full_url) -> str:
     def get_final_id(init_id, digit):
-        init_id = str(init_id)
         final_id = ""
         while digit > 0:
             this_round_digit = len(init_id) if len(init_id) < digit else digit
             final_id += init_id[:this_round_digit]
             digit -= this_round_digit
-        return int(final_id)
+        return final_id
     
     digit = 1
-    init_id = uuid5(NAMESPACE_URL, full_url).int
+    init_id = base62_encode(uuid5(NAMESPACE_URL, full_url).int)
     final_id = get_final_id(init_id, digit)
-    result = db.session.query(url_mapper.url_id).all()
+    result = db.session.query(url_mapper.short_url_id).all()
     id_set = set([id[0] for id in result])
     while final_id in id_set:
         digit += 1
