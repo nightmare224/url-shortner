89feea8348149cee68ae38ca71c2a0c30159105e
Nicole Tai <nicole1999224@gmail.com>
2023-05-04 17:56:52 +0200

    ansible script directory path fix

--- deployment/kubernetes/run.sh
+++ deployment/kubernetes/run.sh
@@ -13,5 +13,5 @@ taglist=`sed -n 's/\[v\] *//pg' taglist | tr '\n' ',' | sed 's/,$//'`
 if [ -z "${taglist}" ]; then
 	log "INFO" -e 'No task to do.\nPlease select the task you want to do in "taglist" file.'
 else
-  ansible-playbook playbook.yaml -i inventory --skip-tags always --tags ${taglist},ansible-init -e 'ansible_python_interpreter=/usr/bin/python3'
+  ansible-playbook ansible/playbook.yaml -i ansible/hosts --skip-tags always --tags ${taglist},ansible-init -e 'ansible_python_interpreter=/usr/bin/python3'
 fi
